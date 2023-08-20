45a9a91dd8f1512a06d1aae7de7006eada1ecb81
Ibrahim Kamay <ibrahimkamayweb@gmail.com>
2023-08-21 00:48:42 +0300

    updated

--- harf-notu-hesaplama/index.js
+++ harf-notu-hesaplama/index.js
@@ -6,59 +6,46 @@ const submitBtn = document.querySelector(".btn-hesapla");
 const resetBtn = document.querySelector(".btn-reset");
 const ortPuanText = document.querySelector(".ortalama");
 const harfNotu = document.querySelector(".harf-notu");
-submitBtn.addEventListener("click", ()=> {
+submitBtn.addEventListener("click", () => {
+  const ortalama = vize.value * 0.3 + odev.value * 0.1 + final.value * 0.6;
 
-const ortalama = (vize.value * 0.3) + (odev.value * 0.1) + (final.value * 0.6);
-
-
-if(ortalama > 90 ) {
-ortPuanText.innerText = `Ortalama Puan : ${ortalama}`;
-harfNotu.innerText =  `Harf Notu: AA`;
-harfNotu.style.color="green";
-}
-
-else if (ortalama >80) {
+  if (ortalama > 90) {
+    ortPuanText.innerText = `Ortalama Puan : ${ortalama}`;
+    harfNotu.innerText = `Harf Notu: AA`;
+    harfNotu.style.color = "green";
+  } else if (ortalama > 80) {
     ortPuanText.innerText = `Ortalama Puan : ${ortalama.toFixed(2)}`;
-harfNotu.innerText =  `Harf Notu: BA`;
-harfNotu.style.color="green";
-}
-else if (ortalama >70) {
+    harfNotu.innerText = `Harf Notu: BA`;
+    harfNotu.style.color = "green";
+  } else if (ortalama > 70) {
     ortPuanText.innerText = `Ortalama Puan : ${ortalama.toFixed(2)}`;
-harfNotu.innerText =  `Harf Notu: BB`;
-harfNotu.style.color="green";
-}
-else if (ortalama >60) {
+    harfNotu.innerText = `Harf Notu: BB`;
+    harfNotu.style.color = "green";
+  } else if (ortalama > 60) {
     ortPuanText.innerText = `Ortalama Puan : ${ortalama.toFixed(2)}`;
-harfNotu.innerText =  `Harf Notu: CB`;
-harfNotu.style.color="orange";
-}
-else if (ortalama >50) {
+    harfNotu.innerText = `Harf Notu: CB`;
+    harfNotu.style.color = "orange";
+  } else if (ortalama > 50) {
     ortPuanText.innerText = `Ortalama Puan : ${ortalama.toFixed(2)}`;
-harfNotu.innerText =  `Harf Notu: CC`;
-harfNotu.style.color="orange";
-}
-else if (ortalama >40) {
+    harfNotu.innerText = `Harf Notu: CC`;
+    harfNotu.style.color = "orange";
+  } else if (ortalama > 40) {
     ortPuanText.innerText = `Ortalama Puan : ${ortalama.toFixed(2)}`;
-harfNotu.innerText =  `Harf Notu: DC`;
-harfNotu.style.color="red";
-}
-else if (ortalama >40) {
+    harfNotu.innerText = `Harf Notu: DC`;
+    harfNotu.style.color = "red";
+  } else if (ortalama > 40) {
     ortPuanText.innerText = `Ortalama Puan : ${ortalama.toFixed(2)}`;
-harfNotu.innerText =  `Harf Notu: DD`;
-harfNotu.style.color="red";
-}
-else{
+    harfNotu.innerText = `Harf Notu: DD`;
+    harfNotu.style.color = "red";
+  } else {
     ortPuanText.innerText = `Ortalama Puan : ${ortalama.toFixed(2)}`;
-harfNotu.innerText =  `Harf Notu: FF`;
-harfNotu.style.color="red";
-}
-})
+    harfNotu.innerText = `Harf Notu: FF`;
+    harfNotu.style.color = "red";
+  }
+});
 
 resetBtn.addEventListener("click", () => {
-
-ortPuanText.innerText = `Ortalama Puan : --`;
-harfNotu.innerText =  `Harf Notu : --`;
-harfNotu.style.color="black";
-})
-
-console.log(resetBtn);
+  ortPuanText.innerText = `Ortalama Puan : --`;
+  harfNotu.innerText = `Harf Notu : --`;
+  harfNotu.style.color = "black";
+});
---
+++ harf-notu-hesaplama/readme.md
@@ -0,0 +1,7 @@
+# Harf Notu Hesaplama Programı
+
+Bu projede vize, ödev ve final notunu kendi yüzdelerinden hesaplayak harf notu hesaplayan programı yazdım. Puan aralığına göre harf notunu yeşil, kırmızı ve turuncu olarak gösteriyor.
+
+Önizleme;
+
+![Logo](https://img001.prntscr.com/file/img001/ALiiSlCpT4uu8z1p2zolgw.jpeg)
